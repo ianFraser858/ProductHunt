f220cf3dc8f057b952b650fc065e165c8c386be1
Ian Fraser <33810966+ianFraser858@users.noreply.github.com>
2018-07-17 12:21:25 -0500

    added registration functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
-"""ProductHunt URL Configuration
 
+"""portfolio_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from products import views
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls')),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
 ]
