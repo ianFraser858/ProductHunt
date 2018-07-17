549c5f9f3f5e8609c27a33634e5c42497d77af57
Ian Fraser <33810966+ianFraser858@users.noreply.github.com>
2018-07-17 16:18:50 -0500

    added product model. Began adding product form

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.TextField(max_length=200)
+    product_url = models.URLField(default='none')
+    pub_date = models.DateTimeField(default=timezone.now)
+    vote_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField(max_length=200)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %y')
