05a6851977ceb06c8e6f1804ec01ec3d559086bd
Ian Fraser <33810966+ianFraser858@users.noreply.github.com>
2018-07-18 14:33:35 -0500

    finished functionality. needs more styling

--- products/models.py
+++ products/models.py
@@ -17,5 +17,8 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def summary(self):
+        return self.body[:100]
+
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %y')
